f303b2f797c5741b1475d0c0c546da8aafa02a1f
efillipi <e.fillipi@gmail.com>
2023-01-10 10:51:19 -0300

    return function

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -11,7 +11,7 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: 'settings.html',
 })
 export class SettingsPage {
- //these variables are used to store data into storage and display the same data on the settings page
+  //these variables are used to store data into storage and display the same data on the settings page
   newCountryID: number;
   countryID: number;
   newMinAge: number;
@@ -23,8 +23,8 @@ export class SettingsPage {
   statistics: any[];
 
 
-   constructor(public navCtrl: NavController, public http: HttpClient, public navParams: NavParams, private st: StatsProvider, public storage: Storage) {
-  
+  constructor(public navCtrl: NavController, public http: HttpClient, public navParams: NavParams, private st: StatsProvider, public storage: Storage) {
+
   }
   //this function is used to get data from a JSON object
   // ionViewDidLoad() {
@@ -40,34 +40,36 @@ export class SettingsPage {
     ).subscribe(data => {
       this.statistics = data.data;
     });
+
+    console.log(this.statistics)
   }
 
 
 
   //this function is used to access stored data from storage
-  ionViewWillEnter(){
+  ionViewWillEnter() {
     this.storage.get("countryID")
-    .then((val) =>{
-      this.countryID = val;
-  
-    })
-    .catch((error) => {
-      alert("Error accessing Storage")
-    })
+      .then((val) => {
+        this.countryID = val;
+
+      })
+      .catch((error) => {
+        alert("Error accessing Storage")
+      })
     this.storage.get("maxAge")
-    .then((val) =>{
-      this.maxAge = val;
-    })
-    .catch((error) => {
-      alert("Error accessing Storage")
-    })
+      .then((val) => {
+        this.maxAge = val;
+      })
+      .catch((error) => {
+        alert("Error accessing Storage")
+      })
     this.storage.get("minAge")
-    .then((val) =>{
-      this.minAge = val;
-    })
-    .catch((error) => {
-      alert("Error accessing Storage")
-    })
+      .then((val) => {
+        this.minAge = val;
+      })
+      .catch((error) => {
+        alert("Error accessing Storage")
+      })
   }
   //this function opens the home page
   openHomePage() {
@@ -77,22 +79,21 @@ export class SettingsPage {
   //this function saves the values enetered into user input forms into storage
   saveIntoStorage() {
     //if a country ID value isn't enetered, an alert box appears
-    if (this.newCountryID == null){
+    if (this.newCountryID == null) {
       alert("Plese enter ID");
 
-    }else{
-    this.countryID = this.newCountryID;
-    this.newCountryID = null;
-    this.minAge = this.newMinAge;
-    this.newMinAge = null;
-    this.maxAge = this.newMaxAge;
-    this.newMaxAge = null;
-    this.storage.set("countryID", this.countryID);
-    this.storage.set("maxAge", this.maxAge);
-    this.storage.set("minAge", this.minAge);
-    
-        
+    } else {
+      this.countryID = this.newCountryID;
+      this.newCountryID = null;
+      this.minAge = this.newMinAge;
+      this.newMinAge = null;
+      this.maxAge = this.newMaxAge;
+      this.newMaxAge = null;
+      this.storage.set("countryID", this.countryID);
+      this.storage.set("maxAge", this.maxAge);
+      this.storage.set("minAge", this.minAge);
+      this.ionViewDidLoad()
+    }
   }
-  }
- 
+
 }
--- src/providers/stats/stats.ts
+++ src/providers/stats/stats.ts
@@ -13,7 +13,7 @@ export class StatsProvider {
 
   constructor(public http: HttpClient, public storage: Storage) {
   }
-  
+
 
   //These variables present the API values
   //I couldn't connect them to storage so the values have to be changed manually to display player info in the table
@@ -23,7 +23,7 @@ export class StatsProvider {
 
   //This function fecthes the data from the API website
   // getStats(): Observable<any> {
-    
+
   //  return this.http.get("https://app.sportdataapi.com/api/v1/soccer/players?apikey=aa1c1a00-886e-11ed-9927-3fd1f0361d34&country_id="
   //   + this.countryID + "&max_age=" + this.maxAge + "&min_age=" + this.minAge);
 
@@ -31,14 +31,19 @@ export class StatsProvider {
 
   getStats(countryID: number, maxAge: number, minAge: number): Observable<any> {
 
+    console.log('getStats')
+    console.log('countryID', countryID)
+    console.log('maxAge', maxAge)
+    console.log('minAge', minAge)
+    
     return this.http.get("https://app.sportdataapi.com/api/v1/soccer/players?apikey=aa1c1a00-886e-11ed-9927-3fd1f0361d34&country_id="
       + countryID + "&max_age=" + maxAge + "&min_age=" + minAge);
   }
 
 
-  }
-  
+}
 
-  
+
+
 
 
